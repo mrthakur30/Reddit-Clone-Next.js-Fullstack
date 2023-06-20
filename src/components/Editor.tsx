842e920c81e6ad161ce7099df4b36e443f5fdf5f
Mukul Thakur <95705739+mrthakur30@users.noreply.github.com>
2023-06-20 16:22:00 +0530

    comnjdf

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -171,7 +171,7 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
     }
   }, [isMounted, initializeEditor])
 
-  async function onSubmit(data: any) {
+  async function onSubmit(data: FormData) {
     const blocks = await ref.current?.save()
 
     const payload: PostCreationRequest = {
